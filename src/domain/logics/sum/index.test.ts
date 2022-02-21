9ae4cbb86165470891919089ff36ca386fc3c4cb
淺井晴登 <>
2022-02-21 15:44:52 +0900

    update:ロジックのテストが出来るようjestの設定を追加

---
+++ src/domain/logics/sum/index.test.ts
@@ -0,0 +1,5 @@
+import { sumNumber } from "./";
+
+test("sumNumber test, 1 + 2 to equail 3", () => {
+  expect(sumNumber(1, 2)).toBe(3);
+});
---
+++ src/domain/logics/sum/index.ts
@@ -0,0 +1,11 @@
+/**
+ * 合計メソッド
+ * @param {number} a
+ * @param {number} b
+ * @returns {number} 合計値
+ */
+const sumNumber = (a: number, b: number) => {
+  return a + b;
+};
+
+export { sumNumber };
