136879a6760826e9ece86c920505865e172ff5e6
淺井晴登 <>
2022-02-24 03:23:24 +0900

    update:Eslintのルールを追加

--- .eslintrc.js
+++ .eslintrc.js
@@ -2,25 +2,32 @@ module.exports = {
   env: {
     browser: true,
     es2021: true,
-    'jest/globals': true
+    "jest/globals": true
   },
   extends: [
-    'standard'
+    "standard"
   ],
-  parser: '@typescript-eslint/parser',
+  parser: "@typescript-eslint/parser",
   parserOptions: {
-    ecmaVersion: 'latest',
-    sourceType: 'module'
+    ecmaVersion: "latest",
+    sourceType: "module"
   },
   plugins: [
-    '@typescript-eslint',
-    'jest'
+    "@typescript-eslint",
+    "jest"
   ],
   rules: {
+    quotes: ["error", "double"],
+    semi: ["error", "always"],
+    "semi-spacing": ["error", { after: true, before: false }],
+    "semi-style": ["error", "last"],
+    "no-extra-semi": "error",
+    "no-unexpected-multiline": "error",
+    "no-unreachable": "error"
     // 'jest/no-disabled-tests': 'warn',
     // 'jest/no-focused-tests': 'error',
     // 'jest/no-identical-title': 'error',
     // 'jest/prefer-to-have-length': 'warn',
     // 'jest/valid-expect': 'error'
   }
-}
+};
