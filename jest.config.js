041284667b1fcd4020fbe8871fdf5d73ab39b595
淺井晴登 <>
2022-02-24 05:32:20 +0900

    update:jestとtsの設定ファイルを編集

--- jest.config.js
+++ jest.config.js
@@ -1,6 +1,6 @@
 /** @type {import('ts-jest/dist/types').InitialOptionsTsJest} */
 module.exports = {
-  preset: 'ts-jest',
-  testEnvironment: 'node',
-  roots: ['src']
-}
+  preset: "ts-jest",
+  testEnvironment: "node",
+  roots: ["src", "utilities"]
+};
